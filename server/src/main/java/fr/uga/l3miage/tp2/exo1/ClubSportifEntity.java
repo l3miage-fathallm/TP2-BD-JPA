441e7c6fc257b830e842cfa48c301ca663bc329b
l3miage-fathallm <mariam.fathallah@etu.univ-grenoble-alpes.fr>
2024-03-14 02:53:05 +0100

    modélisation des classes avec des relations @ManyToMany dans le mode create

---
+++ server/src/main/java/fr/uga/l3miage/tp2/exo1/ClubSportifEntity.java
@@ -0,0 +1,18 @@
+package fr.uga.l3miage.tp2.exo1;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "ClubSportif")
+public class ClubSportifEntity {
+@Id
+    private Long id;
+private String nom;
+private String description;
+@ManyToMany
+    private Set<EtudiantEntity>etudiants;
+}
